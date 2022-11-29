71a93705d76512e124aa1fa15feeac6adb7857f8
Nanzi Mustafa Chowdhury <ge42poj@mytum.de>
2022-11-29 14:21:15 +0600

    setting database connection

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
+require('dotenv').config();
+
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mtryllz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    const categoryCollection = client.db('resaleProducts').collection('categories');
+
+    app.get('/categories', async(req,res) =>{
+      const query = {};
+      const cursor = categoryCollection.find(query);
+      const categories = await cursor.toArray();
+      res.send(categories);
+
+    })
+  }
+  finally{
+
+  }
+}
+run().catch(error => console.log(error));
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
